c338e7a7a475529b55bdec59590b511b0656548f
vickykrsingh27 <vickykrsingh27@gmail.com>
2025-02-13 19:28:48 +0530

    env setup for deployment

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5173'
+    origin:process.env.CLIENT_URL
 }));
 
 async function dbConnect(){
